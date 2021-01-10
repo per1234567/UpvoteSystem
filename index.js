16570509bf74376236422e28a7688d85542452ed
Per Skullerud <pskullerud@hotmail.com>
2021-01-10 12:03:48 +0200

    hello2

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const server = app.listen(PORT, () => {
 
 io = require('socket.io')(server);
 
-const url = 'mongodb+srv://Per:qrQGZSyXmQMl6htf@perliukai-4jawx.gcp.mongodb.net/data?retryWrites=true&w=majority';
+const url = 'mongodb+srv://Per:qrQGZSyXmQMl6htf@perliukai-4jawx.gcp.mongodb.net/perliukai?retryWrites=true&w=majority';
 //const url = 'mongodb://localhost:27017/duomenys';
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
@@ -65,6 +65,7 @@ DB.initialize();
 
 app.get('/', async(req, res) => {
     const data = await DB.get();
+    console.log(data);
     res.render('page', {data : data});
 });
 
